449c51753b416d194c42139822d9e2a911e46bfd
Pranav Nikam <dambepranav15@gmail.com>
2025-05-24 18:09:03 -0400

    if statement

---
+++ 02_Conditionals_and_Loops/01_if_stmt.py
@@ -0,0 +1,8 @@
+# Use 'if' when you want to run a block of code only if a condition is true.
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You're eligible to vote.")
+
+# If the condition is false, Python just skips the block.
