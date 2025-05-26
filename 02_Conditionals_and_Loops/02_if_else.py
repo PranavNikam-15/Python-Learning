5fd3d7a6b8ae5fd48f1bf62d98c409e8ee19e80e
Pranav Nikam <dambepranav15@gmail.com>
2025-05-25 21:34:14 -0400

    if-else condition

---
+++ 02_Conditionals_and_Loops/02_if_else.py
@@ -0,0 +1,8 @@
+# When you want to do one thing if a condition is true, and something else if it’s false.
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
