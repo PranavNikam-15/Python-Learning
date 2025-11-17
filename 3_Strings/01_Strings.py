1fe382785660e51d819371814677f39c5daba4aa
Pranav Nikam <pranavnikam15pn@gmail.com>
2025-11-17 23:03:42 +0530

    String

---
+++ 3_Strings/01_Strings.py
@@ -0,0 +1,15 @@
+'''
+
+A string in Python is an ordered sequence of characters enclosed inside 
+single quotes (' '), double quotes (" ") or triple quotes (''' ''' or """ """).
+
+'''
+
+name = 'PRANAV'
+
+Name = "Pranav Nikam"
+
+paragraph = """This is a 
+multiline string. """
+
+print(name, Name, paragraph, sep="\n")
