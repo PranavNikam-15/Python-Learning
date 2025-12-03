fbefc8f946b737026e8e465d412e348a7d257f99
Pranav Nikam <pranavnikam15pn@gmail.com>
2025-12-04 00:06:21 +0530

    Lambda Function

---
+++ 04_Functions/03_lambda.py
@@ -0,0 +1,11 @@
+'''
+Lambda Function : 
+ - It is a small, anonymous (unnamed) function defined using 'lambda' function. 
+
+'''
+
+square = lambda x : x*x
+print(square(15))
+
+add = lambda a, b: a + b
+print(add(15, 5))
