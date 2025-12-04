2907791fcec8535ca1fde78c2da4b559a4027ff0
Pranav Nikam <pranavnikam15pn@gmail.com>
2025-12-04 23:06:05 +0530

    Modules-1

---
+++ 04_Functions/Modules_1.py
@@ -0,0 +1,13 @@
+'''
+
+Module : Module is simply a Python file (.py) that contains functions, variables, classes, or executable code,
+         which can be imported and reused in other Python programs.
+
+Documentation : https://docs.python.org/3/py-modindex.html
+
+'''
+
+def say_hello(name="Guest"):
+    print("Hello,", name)
+
+pi = 3.14
