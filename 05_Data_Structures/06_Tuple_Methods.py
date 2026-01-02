4130c5b323ba002d883852f478c17bdac1f823fa
Pranav Nikam <pranavnikam15pn@gmail.com>
2026-01-02 11:03:20 +0530

    Tuple Methods

---
+++ 05_Data_Structures/06_Tuple_Methods.py
@@ -0,0 +1,8 @@
+# Creating a tuple
+tu = (10, 20, 30, 20, 40, 20)
+
+# count(value): Returns the number of times the element occurs in the tuple
+print(tu.count(20))
+
+# index(value): Returns the index of the first occurrence of the specified value
+print(tu.index(20))
