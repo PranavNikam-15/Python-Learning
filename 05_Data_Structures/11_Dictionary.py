7c978db3b556404a3dafc3d10186aca02db69f9d
Pranav Nikam <pranavnikam15pn@gmail.com>
2026-01-03 10:57:54 +0530

    Dictionary

---
+++ 05_Data_Structures/11_Dictionary.py
@@ -0,0 +1,15 @@
+# A Dictionary is an ordered, mutable collection of data that stores values in key-value pairs
+
+marks = { "pranav" : 75 }
+print(marks)
+
+# Accessing dic element
+print(marks["pranav"])
+
+# Modifying dictionary
+marks["pranav"] = 80
+
+# Adding new key:value
+marks["buddy"] = 85
+
+print(marks)
